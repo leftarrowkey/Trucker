0a27b7267c4f7faaf29d735646d892930530c441
generic user <you@example.com>
2023-07-09 11:01:50 +0100

    why

--- Label.gd
+++ Label.gd
@@ -1,6 +1,5 @@
-extends Sprite2D
+extends Label
 
-var crew = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,4 +8,4 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	move_local_x(delta * 100)
+	pass
